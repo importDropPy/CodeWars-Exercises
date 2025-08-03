eec883690c4e1158b6d2851915388e0b8edd069f
DropPy <workforcemailboxes@gmail.com>
2025-08-03 10:46:37 -0400

    Update Smallest-Integer-in-an-Array.py

--- CodeWars-Exercises/8kyu/Smallest-Integer-in-an-Array.py
+++ CodeWars-Exercises/8kyu/Smallest-Integer-in-an-Array.py
@@ -86,7 +86,7 @@ EXAMPLES:
     Given [34, 15, 88, 2] then your solution will return 2
     Given [34, -345, -1, 100] then your solution will return -345
     You can assume, for the purpose of this kata, that the supplied array will not be empty.
-'''
+                                                                                            '''
 
 
 
@@ -595,6 +595,7 @@ from find_smallest_int.py import YourFunctionName as YourFunctionsNameButShorter
 ####################################################################################################
 ####################################################################################################
                                                                                                     '''
+
 
 
 
